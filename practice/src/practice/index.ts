a197af834e0a83cf96bfe41a4995cc2658be6c22
Prithivi <prithivi@users.noreply.github.com>
2026-03-30 02:19:40 +0545

    feat: enhance RAG chatbot with graph-based workflow and state management for tool invocation

--- practice/src/practice/index.ts
+++ practice/src/practice/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { tool } from "@langchain/core/tools";
 import { z } from "zod";
@@ -12,12 +13,17 @@ import {
   START,
   END,
 } from "@langchain/langgraph";
+import {
+  AIMessage,
+  SystemMessage,
+  ToolMessage,
+} from "@langchain/core/messages";
 
 // Model
 const model = new ChatGoogleGenerativeAI({
   model: MODEL.GEMINI_2_5_FLASH,
   temperature: 0.5,
-  apiKey: process.env.GEMINI_API_KEY,
+  apiKey: process.env.GEMINI_API_KEY ?? process.env.GOOGLE_API_KEY,
 });
 
 // Tools
@@ -67,6 +73,7 @@ const toolsByName = {
 const tools = Object.values(toolsByName);
 const modelWithTools = model.bindTools(tools);
 
+// State Schema
 const MessagesState = new StateSchema({
   messages: MessagesValue,
   llmCalls: new ReducedValue(
@@ -76,3 +83,56 @@ const MessagesState = new StateSchema({
     },
   ),
 });
+
+// Graph Nodes
+const llmCall: GraphNode<typeof MessagesState> = async (state) => {
+  const response = await modelWithTools.invoke([
+    new SystemMessage(
+      "You are a helpful assistant tasked with performing arithmetic on a set of inputs.",
+    ),
+    ...state.messages,
+  ]);
+
+  return {
+    messages: [response],
+    llmCalls: 1,
+  };
+};
+
+const toolNode: GraphNode<typeof MessagesState> = async (state) => {
+  const lastMessage = state.messages.at(-1);
+  console.log("Last message:", lastMessage);
+  if (lastMessage == null || !AIMessage.isInstance(lastMessage)) {
+    return {
+      messages: [],
+    };
+  }
+
+  const result: ToolMessage[] = [];
+  for (const toolCall of lastMessage.tool_calls ?? []) {
+    const tool = toolsByName[toolCall.name as keyof typeof toolsByName];
+    const observation = await tool.invoke(toolCall);
+    result.push(observation);
+  }
+
+  return {
+    messages: result,
+  };
+};
+
+//Graph Workflow
+const graph = new StateGraph(MessagesState)
+  .addNode("llmCall", llmCall)
+  .addNode("toolNode", toolNode)
+  .addEdge(START, "llmCall")
+  .addEdge("llmCall", "toolNode")
+  .addEdge("toolNode", END)
+  .compile();
+
+// Execute the graph with an initial message
+(async () => {
+  const result = await graph.invoke({
+    messages: [{ role: "user", content: "Who is the father of computer?" }],
+  });
+  console.log("Final Result:", result);
+})();
